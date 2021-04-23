5c60288fe2f7b1415b4bbe04ca929ce866d094e3
Andrei Bora <andrei.bora@8x8.com>
2021-04-23 10:01:29 +0300

    add app.json file

--- src/main/java/com/example/demo3/Demo3Application.java
+++ src/main/java/com/example/demo3/Demo3Application.java
@@ -2,32 +2,41 @@ package com.example.demo3;
 
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import static java.lang.String.*;
 
 @SpringBootApplication
 public class Demo3Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Demo3Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Demo3Application.class, args);
+    }
 
 }
 
 
 @RestController
-class HelloController{
+class HelloController {
 
-	@GetMapping(value = "/message")
-	public Message getMessage(){
-		return new Message("hello");
-	}
+    @Autowired
+    private Environment environment;
+
+    @GetMapping(value = "/message")
+    public Message getMessage() {
+        String name = environment.getProperty("HELLO");
+        name = name != null ? name : "andrei";
+        return new Message(format("hello %s", name));
+    }
 }
 
 @Data
 @RequiredArgsConstructor
-class Message{
-	private final String message;
+class Message {
+    private final String message;
 }
